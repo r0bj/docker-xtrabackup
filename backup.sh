5466181cb105114283dc0b517cabe891f37f9b31
rob <rob@unx.pl>
2019-02-16 00:33:29 +0100

    add basic_auth option

--- backup.sh
+++ backup.sh
@@ -2,6 +2,9 @@
 
 prometheus_pushgateway_url=${PUSHGATEWAY_URL:-http://127.0.0.1:9091}
 prometheus_job=${PROMETHEUS_JOB:-xtrabackup}
+override_hostname=${OVERRIDE_HOSTNAME}
+auth_user=${BASIC_AUTH_USER}
+auth_password=${BASIC_AUTH_PASSWORD}
 
 function write_log {
 	echo "`date +'%Y%m%d %H%M%S'`: $1"
@@ -10,10 +13,15 @@ function write_log {
 function notify_prometheus {
 	local success="$1"
 	local duration="$2"
+
+	if [ -n "$auth_user" ] && [ -n "$auth_password" ]; then
+		curl_opts="-u ${auth_user}:$auth_password"
+	fi
+
 	if [ -n "$prometheus_pushgateway_url" ] && [ -n "$prometheus_job" ]; then
 		if [ "$success" -eq 1 ]; then
 			write_log "INFO: notify prometheus: backup success; duration: $duration"
-cat <<EOF | curl -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
+cat <<EOF | curl $curl_opts -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
 # HELP xtrabackup_duration_seconds Duration of xtrabackup
 # TYPE xtrabackup_duration_seconds gauge
 xtrabackup_duration_seconds $duration
@@ -26,7 +34,7 @@ xtrabackup_last_success 1
 EOF
 		else
 			write_log "INFO: notify prometheus: backup failed"
-cat <<EOF | curl -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
+cat <<EOF | curl $curl_opts -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
 # HELP xtrabackup_last_success Success of xtrabackup
 # TYPE xtrabackup_last_success gauge
 xtrabackup_last_success 0
@@ -35,7 +43,12 @@ EOF
 	fi
 }
 
-hostname=$(hostname)
+if [ -n "$override_hostname" ]; then
+	hostname=$override_hostname
+else
+	hostname=$(hostname)
+fi
+
 start_timastamp=$(date +'%s')
 
 bash /xtrabackup.sh
